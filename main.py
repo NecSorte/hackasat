40f31cccf7eddd254840a347e4447c48e8cfc16c
NecSorte <69991710+NecSorte@users.noreply.github.com>
2023-05-16 18:44:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,75 +11,23 @@ from manuf import manuf
 
 app = Flask(__name__)
 
-# There are some "plugins" that are available for addons.  Let's see if they're present
-hasFalcon = False
-hasBluetooth = False
-hasUbertooth = False
+hasOUILookup = False
 
 try:
     from manuf import manuf
     hasOUILookup = True
 except:
     hasOUILookup = False
-    
-#oui db function 
-def getOUIDB():
-    ouidb = None
-    
-    if hasOUILookup:
-        if  os.path.isfile('manuf'):
-            # We have the file but let's not update it every time we run the app.
-            # every 90 days should be plenty
-            last_modified_date = datetime.datetime.fromtimestamp(os.path.getmtime('manuf'))
-            now = datetime.datetime.now()
-            age = now - last_modified_date
-            
-            if age.days > 90:
-                updateflag = True
-            else:
-                updateflag = False
-        else:
-            # We don't have the file, let's get it
-            updateflag = True
-            
-        try:
-            ouidb = manuf.MacParser(update=updateflag)
-        except:
-            print("Error updating the MAC address database.  Please check if the manuf module needs updating with 'sudo pip3 install --upgrade manuf'.")
-            ouidb = None
-    else:
-        ouidb = None
-        
-    return ouidb
 
-# auto bind usb devices found. 
-def bind_usb_devices():
-    try:
-        output = subprocess.check_output("usbip list -l", shell=True).decode("utf-8")
-        devices = [line.strip() for line in output.split("\n") if "busid" in line]
+def get_network_interfaces():
+    result = os.popen('ip -o link show | awk \'{print $2}\'').read()
+    interfaces = result.split('\n')
+    return [iface.strip(" :") for iface in interfaces if iface]
 
-        for device in devices:
-            busid = device.split(" ")[1]
-            print(f"Binding USB device with busid: {busid}")
-            subprocess.run(f"sudo usbip bind --busid={busid}", shell=True)
-    except Exception as e:
-        print(f"Error while binding USB devices: {e}")
-
-# Call the function to bind the USB devices
-bind_usb_devices()
-
-# lookup serial ports. Specifically any AMC. Needs further testing...) 
 def get_serial_ports():
     ports = [f'/dev/{dev}' for dev in os.listdir('/dev') if dev.startswith('ttyACM') or dev.startswith('ttyUSB')]
     return ports
 
-# lookup wireless devicees
-def get_wireless_interfaces():
-    result = os.popen('iwconfig 2>&1 | grep "^[^ ]" | awk \'{print $1}\'').read()
-    interfaces = result.split('\n')
-    return [iface for iface in interfaces if iface]
-
-# tty device only processes one char at a time. time.sleep(0.01) works. looking to lower the delay moving forward. 
 def send_command(ser, command):
     for char in command:
         ser.write(char.encode())
@@ -89,8 +37,8 @@ def send_command(ser, command):
 @app.route('/')
 def index():
     serial_ports = get_serial_ports()
-    wireless_interfaces = get_wireless_interfaces()
-    return render_template('index.html', serial_ports=serial_ports, wireless_interfaces=wireless_interfaces)
+    network_interfaces = get_network_interfaces()
+    return render_template('index.html', serial_ports=serial_ports, network_interfaces=network_interfaces)
 
 @app.route('/send_command', methods=['POST'])
 def handle_send_command():
