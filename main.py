6d49fa17516c434970a7b2f3c54b108d375cbe58
NecSorte <69991710+NecSorte@users.noreply.github.com>
2023-06-14 18:55:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,52 +6,11 @@ import serial
 import datetime
 import subprocess
 import re
-import threading
 
-import numpy as np
-from threading import Thread
 from flask import Flask, render_template, request, jsonify
+from manuf import manuf
 
 app = Flask(__name__)
-
-# Constants
-AZIMUTH_RANGE = (160, 6400)
-ELEVATION_RANGE = (650, 1450)
-
-# Q-Learning parameters
-alpha = 0.5
-gamma = 0.95
-epsilon = 0.1
-state_space = 10
-action_space = 4
-q_table = np.zeros((state_space, action_space))
-
-# Global state
-current_state = 0
-should_stop = False
-
-# Define a lock
-lock = threading.Lock()
-
-# Define the known devices dictionary
-known_devices = {}
-
-# Define the set of seen MAC addresses
-seen_mac_addresses = set()
-
-# Define the function that modifies known_devices
-def add_or_update_device(device):
-    # Acquire the lock before accessing known_devices
-    with lock:
-        # Do stuff with known_devices here
-        known_devices[device['mac']] = device
-
-# Define the function that reads known_devices
-def get_known_devices():
-    # Acquire the lock before accessing known_devices
-    with lock:
-        # Do stuff with known_devices here
-        return known_devices.copy()  # Return a copy of the known_devices dictionary
 
 hasOUILookup = False
 
@@ -77,81 +36,6 @@ def send_command(ser, command):
         time.sleep(0.01)
     ser.write(b'\r\n')
 
-# Function to extract signal strength from iwlist output
-def parse_signal_strength(output):
-    m = re.search('Signal level=(-?\d+)', output)
-    return int(m.group(1)) if m else None
-
-# Function to get the new position of the antenna based on the current state and action
-def adjust_antenna(state, action):
-    azim = state * ((AZIMUTH_RANGE[1] - AZIMUTH_RANGE[0]) / state_space)
-    elev = ELEVATION_RANGE[0] if action < 2 else ELEVATION_RANGE[1]
-    if action % 2 == 1:
-        azim += (AZIMUTH_RANGE[1] - AZIMUTH_RANGE[0]) / state_space
-    return azim, elev
-
-# Define the function to continuously track a device
-def track_device(mac_address):
-    global should_stop, current_state, known_devices
-    device = known_devices.get(mac_address)
-    if device is None:
-        return
-    while not should_stop:
-        # Choose action
-        if random.uniform(0, 1) < epsilon:
-            action = np.random.choice(action_space)  # Explore action space
-        else:
-            action = np.argmax(q_table[current_state])  # Exploit learned values
-
-        # Take action and get reward
-        azim, elev = adjust_antenna(current_state, action)
-        os.system(f"/send_commands azim {azim}")
-        os.system(f"/send_commands elev {elev}")
-        time.sleep(1)  # Wait for a bit before checking again
-
-        # Find the device in the known_devices dictionary
-        device = known_devices.get(mac_address)
-        if device is None:
-            continue
-
-        # Get the new signal strength from the device
-        new_signal_strength = device.get('signal')
-        reward = new_signal_strength if new_signal_strength else -100
-
-        # Update Q-table
-        old_value = q_table[current_state, action]
-        next_max = np.max(q_table[current_state])
-
-        new_value = (1 - alpha) * old_value + alpha * (reward + gamma * next_max)
-        q_table[current_state, action] = new_value
-
-        # Update current state
-        current_state = int(azim / ((AZIMUTH_RANGE[1] - AZIMUTH_RANGE[0]) / state_space))
-
-# Route to start tracking
-@app.route('/track_device', methods=['POST'])
-def start_tracking():
-    global should_stop
-    should_stop = False
-    mac_address = request.form.get('mac_address')
-    if mac_address is None:
-        return jsonify(success=False, message="mac_address is required"), 400
-
-    devices = get_known_devices()
-    device = devices.get(mac_address)
-    if device is None:
-        return jsonify(success=False, message="Device not found"), 404
-
-    Thread(target=track_device, args=(mac_address,)).start()  # Track the MAC address
-
-    return jsonify(success=True)
-
-@app.route('/stop_tracking', methods=['POST'])
-def stop_tracking():
-    global should_stop
-    should_stop = True
-    return jsonify(success=True)
-
 @app.route('/get_serial_ports', methods=['GET'])
 def get_serial_ports_endpoint():
     return jsonify(get_serial_ports())
@@ -159,7 +43,7 @@ def get_serial_ports_endpoint():
 @app.route('/get_network_interfaces', methods=['GET'])
 def get_network_interfaces_endpoint():
     return jsonify(get_network_interfaces())
-
+    
 @app.route('/')
 def index():
     serial_ports = get_serial_ports()
@@ -170,12 +54,12 @@ def index():
 def handle_send_command():
     port = request.form['port']
     command = request.form['command']
-
+    
     ser = serial.Serial(port, 9600, timeout=1)
     send_command(ser, command)
     response = ser.read(1024).decode()
     ser.close()
-
+    
     return jsonify(response=response)
 
 @app.route('/iwlist', methods=['POST'])
@@ -189,111 +73,53 @@ def handle_iwlist():
 def handle_start_scan():
     port = request.form['port']
     ser = serial.Serial(port, 9600, timeout=1)
-
+    
     for azim in range(170, 6301, 3000):
         command = f'G1 X{azim}'
         send_command(ser, command)
         for elev in range(650, 1401, 500):
             command = f'G1 Y{elev}'
             send_command(ser, command)
-
+    
     ser.close()
     return jsonify(success=True)
 
 @app.route('/wifi_scan', methods=['POST'])
 def handle_wifi_scan():
-    try:
-        interface = request.form['interface']
-        print(f"Scanning on interface: {interface}")  # Debug line
-        output = os.popen(f'sudo iwlist {interface} scan').read()
-        print(output[:100])  # Debug line, print the first 100 characters of the output
-        devices = parse_wifi_scan_output(output)
-        for device in devices.values():
-            add_or_update_device(device)
-        wifi_table_body = render_wifi_details_table(get_known_devices().values())
-        return jsonify(success=True, wifi_table_body=wifi_table_body)
-    except Exception as e:
-        print(e)  # Debug line
-        return jsonify(success=False, error=str(e)), 500
-
-# Define the function to render the Wi-Fi details table
-def render_wifi_details_table(devices):
-    # Clear the table body
-    tbody = ''
-
-    # Iterate over the devices and generate table rows
-    for device in devices:
-        row = f'''
-        <tr>
-            <td>
-                <button class="btn btn-primary track-button">Track</button>
-                <button class="btn btn-danger crack-button">Crack</button>
-                <button class="btn btn-warning ea-button">EA</button>
-            </td>
-            <td>{device['mac']}</td>
-            <td>{device['essid']}</td>
-            <td>{device['mode']}</td>
-            <td>{device['channel']}</td>
-            <td>{device['frequency']}</td>
-            <td>{device['quality']}</td>
-            <td>{device['signal']}</td>
-            <td>{device['noise']}</td>
-            <td>{device['encryption']}</td>
-            <td>{device['device_type']}</td>
-        </tr>
-        '''
-        tbody += row
-
-    return tbody
+    interface = request.form['interface']
+    output = os.popen(f'sudo iwlist {interface} scan').read()
+    devices = parse_wifi_scan_output(output)
+    return jsonify(devices=list(devices.values()))
 
 def parse_wifi_scan_output(output):
-    wifi_networks = []
-    network = {}
+    devices = {}
+    lines = output.split('\n')
 
-    for line in output.split("\n"):
-        if "Cell" in line and network:
-            wifi_networks.append(network)
-            network = {}
-        
-        # MAC Address
-        match = re.search('Address: (.*)', line)
-        if match:
-            network['mac_address'] = match.group(1)
+    p = manuf.MacParser()
 
-        # ESSID
-        match = re.search('ESSID:"(.*)"', line)
-        if match:
-            network['essid'] = match.group(1)
+    for index, line in enumerate(lines):
+        if "Address" in line:
+            mac = line.split()[-1]
+            if mac in devices:  # Skip if device already added
+                continue
 
-        # Mode
-        match = re.search('Mode:(.*)', line)
-        if match:
-            network['mode'] = match.group(1).strip()
-            
-        # Frequency
-        match = re.search('Frequency:(.* GHz)', line)
-        if match:
-            network['frequency'] = match.group(1).strip()
+            device_data = {
+                "mac": mac,
+                "essid": extract_value(lines, index, "ESSID:\"(.*)\""),
+                "mode": extract_value(lines, index, "Mode:(.*)"),
+                "channel": extract_value(lines, index, "Channel:(.*)"),
+                "frequency": extract_value(lines, index, "Frequency:(.*)"),
+                "quality": extract_value(lines, index, "Quality=(.*)"),
+                "signal": extract_value(lines, index, "Signal level=(.*)"),
+                "noise": extract_value(lines, index, "Noise level=(.*)"),
+                "encryption": extract_value(lines, index, "Encryption key:(.*)"),
+                "device_type": p.get_manuf(mac)
+            }
 
-        # Quality
-        match = re.search('Quality=(.*)', line)
-        if match:
-            network['quality'] = match.group(1).strip()
+            devices[mac] = device_data
 
-        # Signal level
-        match = re.search('Signal level=(.*)', line)
-        if match:
-            network['signal_level'] = match.group(1).strip()
+    return devices
 
-        # Encryption
-        match = re.search('Encryption key:(.*)', line)
-        if match:
-            network['encryption'] = match.group(1).strip()
-
-    if network:
-        wifi_networks.append(network)
-
-    return wifi_networks
 
 
 def extract_value(lines, start_index, pattern):
@@ -304,6 +130,29 @@ def extract_value(lines, start_index, pattern):
             return match.group(1)
     return None
 
+
+
+
+@app.route('/array_scan', methods=['POST'])
+def handle_array_scan():
+    port = request.form['port']
+    ser = serial.Serial(port, 9600, timeout=1)
+    
+    azim_min = 170
+    azim_max = 6301
+    elev_min = 650
+    elev_max = 1401
+    step = 500
+
+    for azim in range(azim_min, azim_max + 1, step):
+        command = f'G1 X{azim}'
+        send_command(ser, command)
+        for elev in range(elev_min, elev_max + 1, step):
+            command = f'G1 Y{elev}'
+            send_command(ser, command)
+
+    ser.close()
+    return jsonify(success=True)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
